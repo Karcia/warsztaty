5b40d9e77f7f33effd7093d217738cfbd7419026
Karcia <karciawow@gmail.com>
2015-06-09 23:46:39 +0200

    Add validation to product model, average_rating count

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,15 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+
+  validates_presence_of :title, :description, :price
+  validates_format_of :price, :with => /\A\d+\.*\d{0,2}\z/
+
+  def average_rating
+    ratings = []
+    reviews.each do |r|
+      ratings << r.rating
+    end
+    ratings.sum / ratings.size.to_f
+  end
 end
