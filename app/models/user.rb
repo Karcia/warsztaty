582f870e1cd520c65a49d6aefc7634c2f8eefb7f
Karcia <karciawow@gmail.com>
2015-06-09 20:59:44 +0200

    Add migration to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,12 @@ class User < ActiveRecord::Base
 
   has_many :reviews
   has_many :products
+
+
+  validates_presence_of :firstname
+  validates_presence_of :lastname
+
+  def admin?
+  	is_admin
+  end
 end
