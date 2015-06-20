27a5311f32a1a74525c3059e156d9df12800f8c8
Karcia <karciawow@gmail.com>
2015-06-20 14:46:11 +0200

    Added faker data to seeds.rb; modified ruby gemset;

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'ffaker'
+
+5.times do
+  User.create(email: Faker::Internet.email, password: "haslo1234", firstname: Faker::Name.first_name, lastname: Faker::Name.last_name)
+end
+
+User.create(email: "admin@example.com", password: "admin1234", firstname: "John", lastname: "Doe", is_admin: true)
+
+3.times do
+  Category.create(name: Faker::Color.name)
+end
+
+30.times do
+  p = Product.create(title: Faker::Food.fruit, description: Faker::Lorem.sentence, price: rand(1..100), category_id: Category.order("RANDOM()").first.id, user_id: User.order("RANDOM()").first.id)
+  3.times do
+    Review.create(product: p, user: User.order("RANDOM()").first, content: Faker::Lorem.sentence, rating: rand(1..5))
+  end
+end
